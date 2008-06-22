6471af9617bddc71cba549c784deb93007890021
dclinton <dclinton@553ff346-1e4f-0410-9a96-d18bb9526ef0>
2008-06-22 14:28:13 +0000

    Created friend-feeder directory

---
+++ friend-feeder/finder.py
@@ -0,0 +1,13 @@
+#!/opt/local/bin/python2.5
+
+import sys
+import httl
+FRIENDFEED_URL = 'http://friendfeed.com/%s'
+
+def Main():
+  friendfeed_username = sys.argv[1]
+  
+
+
+if __name__ == '__main__':
+  Main()

818ae3d06f6d652849b9f16e073cf7412c35dd09
dclinton <dclinton@553ff346-1e4f-0410-9a96-d18bb9526ef0>
2009-05-31 17:00:17 +0000

    Copying http://svn.unto.net/svn/public/orchard/

---
+++ orchard/trunk/README.txt
@@ -0,0 +1,4 @@
+This is a placeholder for a proper README on the Orchard project.
+
+Orchard is a server that manages user identity information.
+
---
+++ orchard/trunk/src/main/java/net/unto/orchard/HelloWorldServlet.java
@@ -0,0 +1,14 @@
+package net.unto.orchard;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet
+{
+  public void doGet( HttpServletRequest request, HttpServletResponse response )
+  {
+
+  }
+}
+

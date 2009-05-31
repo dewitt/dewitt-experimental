2727e06ec02ae85b340479869f86f53773b0b720
dclinton <dclinton@553ff346-1e4f-0410-9a96-d18bb9526ef0>
2009-05-31 16:42:54 +0000

    Copying http://svn.unto.net/svn/public/les/.

---
+++ les/trunk/lib/Les.pm
@@ -0,0 +1,23 @@
+package Unto::[PACKAGE_NAME];
+
+use strict;
+$VERSION = "0.1";
+sub Version { $VERSION; }
+
+1;
+
+
+=pod
+
+=head1 NAME
+
+  Unto::[PACKAGE_NAME] -- [Enter Description Here]
+
+=head1 AUTHOR
+
+  Author: [Your name here]
+
+
+Copyright (C) 2005 DeWitt Clinton, All Rights Reserved
+
+=cut
